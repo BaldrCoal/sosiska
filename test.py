dd89c71cbf47dcfd688c5aaaef88ed0b12801aaa
BaldrCoal <vladnilis98@gmail.com>
2024-06-08 19:01:40 +0300

    low l

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import subprocess
-
+from getmac import get_mac_address as gma
 import keyboard
 
 # while not keyboard.is_pressed('e'):
@@ -7,4 +7,5 @@ import keyboard
 
 # subprocess.Popen('dmidecode.exe -s system-uuid'.split())
 current_machine_id = subprocess.check_output('wmic csproduct get uuid').decode().split('\n')[1].strip()
+print(gma())
 print(current_machine_id)
